993a7d8a0bb52007912c2f9f96a424b4d26c0bb2
Mimo <mimosaha@gmail.com>
2018-12-05 19:22:43 +0600

    comment update- 3

--- app/src/main/java/portal/remote/superpeer/remote/com/travisciadd/MainActivity.java
+++ app/src/main/java/portal/remote/superpeer/remote/com/travisciadd/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Travis string has been added
+        // Travis string has been add
     }
 }
